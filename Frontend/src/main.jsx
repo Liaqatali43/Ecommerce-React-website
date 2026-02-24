307860452ce7dafb9f64f8e474fc874ebd52fdb4
munawar ali <munawarsaith12@gmail.com>
2026-02-24 02:11:49 -0800

    figma uri

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const figmad="https://www.figma.com/design/DwiqeZ0yNansrIFvJHH53W/97--Ecommerce-Landing-Page-With-Dashboard--Community-?node-id=6511-7075&t=sp4hQIn2QOJsx8S7-0"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    
   </StrictMode>,
 )
